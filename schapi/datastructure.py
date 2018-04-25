5576cf248b8bfe8ceddefceaaa842e2cd0455c0b
JoMingyu <city7310@naver.com>
2018-04-25 10:53:22 +0900

    UPDATE: Meal 클래스 정의

--- schapi/datastructure.py
+++ schapi/datastructure.py
@@ -1,5 +1,14 @@
 class Meal:
-    pass
+    def __init__(self, breakfast=list(), lunch=list(), dinner=list()):
+        """
+        Args:
+            breakfast (list) : Breakfast menu list
+            lunch (list) : Lunch menu list
+            dinner (list) : Dinner menu list
+        """
+        self.breakfast = breakfast
+        self.lunch = lunch
+        self.dinner = dinner
 
 
 class Schedule:
