4b87b0cb8e1e3d0b141a8665270e7ce8484ce1d3
JoMingyu <city7310@naver.com>
2018-04-25 09:49:06 +0900

    ADD: setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(name='schapi',
+      version='0.1',
+      url='https://github.com/DSM-SchoolAPI/schapi-py',
+      license='Apache License 2.0',
+      author='PlanB',
+      author_email='mingyu.planb@gmail.com',
+      description='Korean School meal API',
+      classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+      ],
+      packages=['schapi'],
+      install_requires=['bs4'])
