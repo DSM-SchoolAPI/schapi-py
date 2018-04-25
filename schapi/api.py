c3b32147fd50aee2612792d0a0536d42b6284d76
JoMingyu <city7310@naver.com>
2018-04-25 10:54:02 +0900

    UPDATE: 급식 정보 핵심 로직 작성

--- schapi/api.py
+++ schapi/api.py
@@ -5,6 +5,8 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
+from .datastructure import Meal
+
 _url = 'http://{0}/sts_sci_md00_001.do?schulCode={1}&schulCrseScCode=4&schulKndScScore=04&schYm={2}{3:0>2}'
 
 
@@ -38,3 +40,66 @@ class SchoolAPI:
         self.region = region.value
         self.school_code = school_code
 
+    def _get_ssl_context(self):
+        context = ssl._create_unverified_context()
+
+        return context
+
+    def _get_menu_dict(self, data):
+        daily_menus = re.findall('[가-힇]+\(\w+\)|[가-힇]+', data)
+
+        menu_dict = dict()
+        timing = [menu for menu in daily_menus if re.match('[조중석]식', menu)]
+        # 조식, 중식, 석식 중 있는 데이터만
+
+        for i in range(len(timing)):
+            if i + 1 >= len(timing):
+                # 마지막 메뉴
+                menu_dict[timing[i]] = daily_menus[daily_menus.index(timing[i]) + 1:]
+            else:
+                menu_dict[timing[i]] = daily_menus[daily_menus.index(timing[i]) + 1: daily_menus.index(timing[i + 1])]
+
+        return menu_dict
+
+    def get_monthly_menus(self, year, month):
+        """
+        Inquire monthly school meals
+
+        Args:
+            year (int) : Year to inquire
+            month (int) : Month to inquire
+
+        Returns:
+            list: Monthly meal list
+        """
+        menus = [Meal()]
+
+        context = self._get_ssl_context()
+        resp = urlopen(_url.format(self.region, self.school_code, year, month), context=context)
+
+        soup = BeautifulSoup(resp, 'html.parser')
+
+        for data in [td.text for td in soup.find(class_='tbl_type3 tbl_calendar').find_all('td') if td.text != ' ']:
+            meal = Meal()
+
+            if len(data) > 1 and data != '자료가 없습니다':
+                menu_dict = self._get_menu_dict(data)
+
+                try:
+                    meal.breakfast = menu_dict.pop('조식')
+                except KeyError:
+                    pass
+
+                try:
+                    meal.lunch = menu_dict.pop('중식')
+                except KeyError:
+                    pass
+
+                try:
+                    meal.dinner = menu_dict.pop('석식')
+                except KeyError:
+                    pass
+
+            menus.append(meal)
+
+        return menus
