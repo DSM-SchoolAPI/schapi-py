cba95302205b79b040642157e2135cbf7edebe76
JoMingyu <city7310@naver.com>
2018-04-25 11:03:39 +0900

    ADD: 급식 정보 테스트 케이스

---
+++ test.py
@@ -0,0 +1,35 @@
+from unittest import TestCase as TC, main
+import time
+
+from schapi import SchoolAPI, Region
+
+
+class APITestBase(TC):
+    def setUp(self):
+        self.api = SchoolAPI(Region.DAEJEON, 'G100000170')
+        
+        super(APITestBase, self).setUp()
+
+
+class TestMeal(APITestBase):
+    def test_monthly_menus(self):
+        meals = self.api.get_monthly_menus(2018, 4)
+
+        self.assertListEqual(
+            meals[3].breakfast,
+            ['기장밥', '어묵국', '비엔나푸실리볶음', '메추리알떡조림', '포도맛요거트', '배추김치']
+        )
+
+        self.assertListEqual(
+            meals[3].lunch,
+            ['기장밥', '순두부찌개', '닭봉데리야끼오븐구이', '실채도라지초무침', '견과류또띠아칩', '석박지']
+        )
+
+        self.assertListEqual(
+            meals[3].dinner,
+            ['기장밥', '청경채된장국', '영양돼지갈비찜', '도토리묵상추무침', '배추김치', '제주감귤쥬스']
+        )
+
+
+if __name__ == '__main__':
+    main()
